16b65226ab451cfcaa11cce632e8e23789356ad6
unknown <wearw133@outlook.com>
2024-05-02 17:44:52 +0600

    l

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,23 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import React from 'react';
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link >Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="header">
+      <div className="container">
+      <a href="#" className="logo">logo</a>
+        <nav className="header_nav">
+          <a href="" className="header_link">Home</a>
+          <a href="" className="header_link">About</a>
+          <a href="" className="header_link">Contacts</a>
+        </nav>
+        <div className="burger-menu">
+        <nav className="header_nav-mobile">
+          <a href="" className="header_link">Home</a>
+          <a href="" className="header_link">About</a>
+          <a href="" className="header_link">Contacts</a>
+        </nav>
+        </div>
+      </div>
+    </div>
   );
 }
 
